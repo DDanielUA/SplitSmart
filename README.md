b70ec5066193781fb1464c7249ac7b298129678b
Shaaryn <deak.daniel16@gmail.com>
2021-11-26 14:45:25 +0100

    Initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SplitSmart
 5-Software Design Project work
+
+Berta Mach
+Daniel Deak 20219579
---
+++ SplitSmart/src/com/SplitSmart/Application/Main.java
@@ -0,0 +1,8 @@
+package com.SplitSmart.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Make your friends pay!");
+    }
+}
