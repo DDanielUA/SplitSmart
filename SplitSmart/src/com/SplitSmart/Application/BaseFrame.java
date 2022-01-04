be9560fad24026c9feba4b771f17197eb72d13cd
Berta Mach <mach.berta@gmail.com>
2022-01-04 11:53:58 +0100

    editing the frame

--- SplitSmart/src/com/SplitSmart/Application/BaseFrame.java
+++ SplitSmart/src/com/SplitSmart/Application/BaseFrame.java
@@ -7,10 +7,14 @@ public class BaseFrame extends JFrame
 {
     BaseFrame()
     {
+        ImageIcon logo = new ImageIcon("logo_png.png");
+
         JLabel slogan = new JLabel(); //you could just add text here
         slogan.setText("Make your friends pay!");
 
         JLabel name = new JLabel("SplitSmart");
+        name.setText("SplitSmart");
+        name.setIcon(logo);
 
         this.setTitle("SplitSmart App");
         this.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
@@ -19,11 +23,10 @@ public class BaseFrame extends JFrame
         this.setVisible(true);
         this.getContentPane().setBackground(new Color(53, 190, 205));
 
-        ImageIcon logo = new ImageIcon("logo_png.png");
         this.setIconImage(logo.getImage()); //changes the image on the frame NOT WORKING
 
         this.add(name);
-        this.add(slogan);
+        //this.add(slogan);
     }
 
 }
