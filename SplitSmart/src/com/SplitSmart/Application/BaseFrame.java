fbea9b8a2d0d11044f03a1e3063ff135dbca9287
Shaaryn <deak.daniel16@gmail.com>
2022-01-06 16:23:04 +0100

    class syntax preference

--- SplitSmart/src/com/SplitSmart/Application/BaseFrame.java
+++ SplitSmart/src/com/SplitSmart/Application/BaseFrame.java
@@ -3,49 +3,33 @@ package com.SplitSmart.Application;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class BaseFrame extends JFrame
 {
-    JButton back;
+    protected final Color _BackgroundColor = new Color(162, 243, 185);
+
+    protected final ImageIcon _Logo = new ImageIcon("src/com/SplitSmart/Images/logo coloured.jpg");
+    protected final ImageIcon _SmallLogo = new ImageIcon(new ImageIcon("src/com/SplitSmart/Images/logo coloured.jpg").getImage().getScaledInstance(75, 75, Image.SCALE_DEFAULT));
+    protected final ImageIcon _GreyLogo = new ImageIcon("src/com/SplitSmart/Images/logo.jpg");
+
+    private JButton backButton;
+
+    private JPanel headerPanel;
+    private JPanel backPanel;
+    private JPanel sloganPanel;
+
+    private JLabel sloganLabel;
+    private JLabel nameLabel;
 
     BaseFrame()
     {
-        //creating basic components
-        ImageIcon logo = new ImageIcon("src/com/SplitSmart/Images/logo coloured.jpg");
-        ImageIcon smallLogo = new ImageIcon(new ImageIcon("src/com/SplitSmart/Images/logo coloured.jpg").getImage().getScaledInstance(75, 75, Image.SCALE_DEFAULT));
-        ImageIcon logoGrey = new ImageIcon("src/com/SplitSmart/Images/logo.jpg");
-        Border buttonBorder = BorderFactory.createEtchedBorder();
-        Color background = new Color(162, 243, 185);
+        FrameSettings();
+        ConstructLabels();
+        ConstructButtons();
+        ConstructPanels();
+    }
 
-        //button
-        back = new JButton();
-        back.setBounds(30, 60, 100, 50);
-        back.addActionListener(e -> System.out.println("back"));
-        back.setText("Back");
-        back.setFocusable(false);
-        back.setBackground(new Color(58, 167, 92));
-        back.setBorder(buttonBorder);
-
-        //slogan label creation and settings
-        JLabel slogan = new JLabel(); //you could just add text here
-        slogan.setText("Make your friends pay!");
-        slogan.setFont(new Font("Consolas", Font.PLAIN, 14));
-
-
-        //name label creation and settings
-        JLabel name = new JLabel("SplitSmart"); //, SwingConstants.CENTER
-        name.setText("SplitSmart"); //"<html>First line<br>Second line</html>"
-        name.setIcon(smallLogo);
-        name.setHorizontalTextPosition(JLabel.CENTER);
-        name.setVerticalTextPosition(JLabel.TOP);
-        name.setFont(new Font("MV Boli", Font.BOLD, 20));
-        name.setVerticalAlignment(JLabel.TOP);
-        name.setHorizontalAlignment(JLabel.CENTER);
-        name.setFont(new Font("Consolas", Font.PLAIN, 14));
-        //name.setBounds(100, 0, 250, 250);
-
+    private void FrameSettings(){
         //setting basic things in the frame
         this.setTitle("SplitSmart App");
         this.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
@@ -53,41 +37,62 @@ public class BaseFrame extends JFrame
         this.setSize(500, 666);
         this.setLayout(null);
         this.setVisible(false);
-        this.getContentPane().setBackground(background);
-        this.setIconImage(logo.getImage()); //changes the image on the frame
+        this.getContentPane().setBackground(this._BackgroundColor);
+        this.setIconImage(_Logo.getImage()); //changes the image on the frame
         //this.setLocationRelativeTo(null);
-
-        //adding panels
-        JPanel backPanel = new JPanel();
-        backPanel.setBackground(background);
-        backPanel.setBounds(0, 0, 100, 120);
-
-        JPanel header = new JPanel();
-        //header.setLayout(new FlowLayout( FlowLayout.CENTER));
-        header.setBackground(background);
-        header.setBounds(100, 0, 300, 120);
-
-        JPanel sloganPanel = new JPanel();
-        //sloganPanel.setLayout( new GridLayout( 2 , 1 ) );  // 2 rows 1 column
-        sloganPanel.setBackground(background);
-        sloganPanel.setBounds(0, 120, 500, 50);
-
-        //header.add(sloganPanel);
-
-        //editing label things in the frame
-
-        this.add(backPanel);
-        this.add(header);
-        this.add(sloganPanel);
-
-        backPanel.add(back);
-        header.add(name);
-        sloganPanel.add(slogan);
-
-        //this.add(name);
-        //this.add(slogan);
-        //this.pack(); //makes the frame as big as components need it to be
     }
 
+    private void ConstructLabels(){
+        //slogan label creation and settings
+        this.sloganLabel = new JLabel(); //you could just add text here
+        sloganLabel.setText("Make your friends pay!");
+        sloganLabel.setFont(new Font("Consolas", Font.PLAIN, 14));
+
+        //name label creation and settings
+        this.nameLabel = new JLabel("SplitSmart"); //, SwingConstants.CENTER
+        nameLabel.setText("SplitSmart"); //"<html>First line<br>Second line</html>"
+        nameLabel.setIcon(this._SmallLogo);
+        nameLabel.setHorizontalTextPosition(JLabel.CENTER);
+        nameLabel.setVerticalTextPosition(JLabel.TOP);
+        nameLabel.setFont(new Font("MV Boli", Font.BOLD, 20));
+        nameLabel.setVerticalAlignment(JLabel.TOP);
+        nameLabel.setHorizontalAlignment(JLabel.CENTER);
+        nameLabel.setFont(new Font("Consolas", Font.PLAIN, 14));
+        //name.setBounds(100, 0, 250, 250);
+    }
+
+    private void ConstructPanels(){
+        this.backPanel = new JPanel();
+        this.add(backPanel);
+        backPanel.setBackground(this._BackgroundColor);
+        backPanel.setBounds(0, 0, 100, 120);
+        backPanel.add(backButton);
+
+        this.headerPanel = new JPanel();
+        this.add(headerPanel);
+        //header.setLayout(new FlowLayout( FlowLayout.CENTER));
+        headerPanel.setBackground(this._BackgroundColor);
+        headerPanel.setBounds(100, 0, 300, 120);
+        headerPanel.add(nameLabel);
+
+        this.sloganPanel = new JPanel();
+        this.add(sloganPanel);
+        //sloganPanel.setLayout( new GridLayout( 2 , 1 ) );  // 2 rows 1 column
+        sloganPanel.setBackground(this._BackgroundColor);
+        sloganPanel.setBounds(0, 120, 500, 50);
+        sloganPanel.add(sloganLabel);
+    }
+
+    private void ConstructButtons(){
+        Border buttonBorder = BorderFactory.createEtchedBorder();
+
+        this.backButton = new JButton();
+        backButton.setBounds(30, 60, 100, 50);
+        backButton.addActionListener(e -> System.out.println("back"));
+        backButton.setText("Back");
+        backButton.setFocusable(false);
+        backButton.setBackground(new Color(58, 167, 92));
+        backButton.setBorder(buttonBorder);
+    }
 
 }
