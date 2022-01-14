476911ede26a5fdd711cccdb0046e67b20435346
Shaaryn <deak.daniel16@gmail.com>
2022-01-14 21:52:46 +0100

    Created transparent icons

--- SplitSmart/src/com/SplitSmart/Application/Config.java
+++ SplitSmart/src/com/SplitSmart/Application/Config.java
@@ -13,7 +13,7 @@ public final class Config {
     public static final Font _BaseFont = new Font("Consolas", Font.PLAIN, 14);
     public static final Font _ErrorFont = new Font("Consolas", Font.PLAIN, 14);
 
-    public static final ImageIcon _Logo = new ImageIcon("src/com/SplitSmart/Images/logo coloured.jpg");
-    public static final ImageIcon _SmallLogo = new ImageIcon(new ImageIcon("src/com/SplitSmart/Images/logo coloured.jpg").getImage().getScaledInstance(75, 75, Image.SCALE_DEFAULT));
+    public static final ImageIcon _Logo = new ImageIcon("src/com/SplitSmart/Images/logo_coloured_tp.png");
+    public static final ImageIcon _SmallLogo = new ImageIcon(new ImageIcon("src/com/SplitSmart/Images/logo_coloured_tp.png").getImage().getScaledInstance(75, 75, Image.SCALE_DEFAULT));
     public static final ImageIcon _CheckMark = new ImageIcon(new ImageIcon("src/com/SplitSmart/Images/checkmark.jpg").getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT));
 }
